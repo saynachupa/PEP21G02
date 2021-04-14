4f47c1f6226d522b4f78bd633fcdeb1fa9f5d749
FlorinLincar(mod1) <florin.lincar@yahoo.com>
2021-04-14 21:27:20 +0300

    add modul1

---
+++ modul1.py
@@ -0,0 +1,11 @@
+print('hello world')
+print('print\u200b("")')
+#print​("")
+HELLO_WORLD = 'Hello world'
+print(HELLO_WORLD)
+
+name = 'Chupa'
+#print(name,name,name, sep='#', end='')
+#print(100* (name+ '\n'))
+#print(4*'3')
+print(4*(4*'*'+'\n'))
